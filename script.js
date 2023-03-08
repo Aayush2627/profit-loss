d057ecce3bfddc5b3b26728be796ac98e61c3dcd
Aayush Sharma <aaayushhh20@gmail.com>
2023-03-08 20:05:55 +0530

    added files

---
+++ script.js
@@ -0,0 +1,39 @@
+const initial=document.querySelector("#initial-price");
+const quantity=document.querySelector("#quantity");
+const current=document.querySelector("#current-price");
+const button=document.querySelector(".button");
+const output=document.querySelector(".output");
+button.addEventListener('click', clickHandler);
+function clickHandler(){
+    var initialPrice=initial.value;
+    var currentPrice=current.value;
+    var quantityValue=quantity.value;
+    if(initialPrice&&currentPrice&&quantityValue){
+    var profitLoss=calculateProfitAndLoss(initialPrice,currentPrice,quantityValue);
+    output.innerText=profitLoss;
+    if(currentPrice>initialPrice){
+        output.style.color='green'
+    }else if(initialPrice>currentPrice){
+        output.style.color='red';
+    }
+    else{
+        output.style.color='gray';
+    }
+}else{
+        output.innerText="Please Enter all values first!!"
+    }
+}
+function calculateProfitAndLoss(cost ,sell,qty){
+    if(cost>sell){
+        const loss=(cost-sell)*qty;
+        const lossPercent=(loss/(cost*qty))*100;
+        return `The Loss is ${loss} and the loss percentage is ${lossPercent.toFixed(2)}%`;
+    }else if(sell>cost){
+        const profit=(sell-cost)*qty;
+        const profitPercent=(profit/(cost*qty))*100;
+        return `The Profit is ${profit} and the profit percentage is ${profitPercent.toFixed(2)}%`;
+    }else{
+        return `Bro you are stuck`;
+    }
+    
+}
